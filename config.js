6df355e7bcb53be328bb0762c9c44f2d1c6bf6fb
Arpit Somani <arpit.somanias8@gmail.com>
2020-08-01 23:19:36 +0530

    Passport Facebook"

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '269851044274379',
+        clientSecret: '0216636eeaa433ae04d60c94038f35a8'
+    }
 }
--- models/user.js
+++ models/user.js
@@ -11,12 +11,21 @@ var User = new Schema({
       type: String,
         default: ''
     },
+    facebookId: String,
     admin:   {
         type: Boolean,
         default: false
     }
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
 
 User.plugin(passportLocalMongoose);
 
